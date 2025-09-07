05ef859d5ad8068b3a73d0acd769ea49f0324522
NaveenBuidl <naveenbuidl@gmail.com>
2025-09-07 20:19:39 +0200

    fixed axios issue

--- client/src/apicalls/index.js
+++ client/src/apicalls/index.js
@@ -3,12 +3,9 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 export const axiosInstance = axios.create({
-  // baseURL: "https://academy-june-25.onrender.com",
-  baseURL: "http://localhost:5000",
+  baseURL: "",
   headers: {
-    withCredentials: true,
-    "Content-Type": "application/json",
-    Authorization: token ? `Bearer ${token}` : "",
+    "Content-Type": "application/json"
   },
 });
 
